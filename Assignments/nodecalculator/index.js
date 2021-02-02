cbe7055ca186ba0abd977409f79fe133b5631a23
Lizadel Yarisantos <63756348+deliscool@users.noreply.github.com>
2021-02-01 23:11:35 -0700

    Add files via upload

--- Assignments/nodecalculator/index.js
+++ Assignments/nodecalculator/index.js
@@ -1,1 +1,21 @@
+var readlineSync = require('readline-sync');
 
+var num1 = readline.question('What is your first number? ')
+var num2 = readline.question('What is your second number?')
+var question = readline.question('Which operation should I perform?');
+
+function calculateAnswer (num1, num2) {
+    if(ops[answer] === '+'){
+        console.log('The answer is ' + num1 + num2 +'!')
+    }
+    else if(ops[answer] === '-'){
+        console.log('The answer is ' + num1 - num2 +'!')
+    }
+    else if(ops[answer] === 'x'){
+        console.log('The asnwer is ' + num1 * num2 +'!')
+    }
+    else {
+        console.log("The asnwer is" + num1 / num2 +'!')
+    }
+calculateAnswer(num1,num2)
+ 
