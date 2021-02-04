8af19dfeaf8790d42cee476707f255b0cf47c75a
Lizadel Yarisantos <63756348+deliscool@users.noreply.github.com>
2021-02-03 23:10:56 -0700

    Fixed bugs

--- Assignments/nodecalculator/index.js
+++ Assignments/nodecalculator/index.js
@@ -1,21 +1,23 @@
-var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync')
 
-var num1 = readline.question('What is your first number? ')
-var num2 = readline.question('What is your second number?')
-var question = readline.question('Which operation should I perform?');
+var num1 = readlineSync.question('What is your first number?');
+var num2 = readlineSync.question('What is your second number?');
+
+var question = readlineSync.question('Which operation should I perform (+,-,*,/)?');
 
 function calculateAnswer (num1, num2) {
-    if(ops[answer] === '+'){
-        console.log('The answer is ' + num1 + num2 +'!')
+    if(question === '+'){
+        console.log('Your answer is ' + (Number(num1) + Number(num2)))
+        }
+    else if(question === '-'){
+        console.log('Your answer is ' + (Number(num1) - Number(num2)))
+        }
+    else if(question === '*'){
+        console.log('Your answer is ' + (Number(num1) * Number(num2)))
+        }
+    else if (question === '/'){ 
+        console.log('Your answer is ' + (Number(num1) / Number(num2)))  
+        }
     }
-    else if(ops[answer] === '-'){
-        console.log('The answer is ' + num1 - num2 +'!')
-    }
-    else if(ops[answer] === 'x'){
-        console.log('The asnwer is ' + num1 * num2 +'!')
-    }
-    else {
-        console.log("The asnwer is" + num1 / num2 +'!')
-    }
-calculateAnswer(num1,num2)
- 
+    
+   calculateAnswer(num1,num2)
