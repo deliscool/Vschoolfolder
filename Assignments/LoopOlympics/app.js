48acd1164702224d407f2a05b17773ffc7a46419
Lizadel Yarisantos <yarisantos@gmail.com>
2021-02-19 21:50:32 -0700

    practice

--- Assignments/LoopOlympics/app.js
+++ Assignments/LoopOlympics/app.js
@@ -1,1 +1,15 @@
+//var fruit = ["banana", "orange", "apple", "kiwi"]
 
+//for (let i = 0; i < 2; i++) {
+//    if (fruit[i] === "no") {
+//    console.log(vegetable[i].experience)
+//    }
+// }
+// loop that prints to the console the numbers 0 through 9
+//for (let i = 0; i <= 9; i++) {
+//    console.log(i)
+//    }
+
+for (let i = 9; i <= 9; i++) {
+    console.log(i)
+    }
