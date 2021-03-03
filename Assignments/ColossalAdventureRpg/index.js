67d35e13892a7f67f4ce90e203817a5c3923b7dc
Lizadel Yarisantos <yarisantos@gmail.com>
2021-03-03 12:50:57 -0700

    Fix edits

--- Assignments/ColossalAdventureRpg/index.js
+++ Assignments/ColossalAdventureRpg/index.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 
 
 let gameOver = false;
-let hasWon = false;
 let playerHealth = 100;
 let backpack = [];
 let animal = [];
+
 
 
 function Animals (animalName, minDamage, maxDamage, HP, item) {
@@ -19,10 +19,10 @@ function Animals (animalName, minDamage, maxDamage, HP, item) {
 }
 
 
-animal.push(new Animals("a team of monkeys", 40, 70, 50, "Banana Peel"));
-animal.push(new Animals("a Lioness", 60, 90, 70, "Claws"));
-animal.push(new Animals("a poisionous dart Frog", 70, 90, 90, "Poison Potion"));
-animal.push(new Animals("a flesh eating tree", 50, 70, 130, "Beating Staff"));
+animal.push(new Animals("a team of monkeys 🙊", 40, 70, 50, "Banana Peel"));
+animal.push(new Animals("a Lioness 🦁", 60, 90, 70, "Claws"));
+animal.push(new Animals("a poisionous dart Frog 🐸", 70, 90, 90, "Poison Potion"));
+animal.push(new Animals("a flesh eating tree 😈", 50, 70, 130, "Beating Staff"));
 
 //Game introduction, name and start request
 
@@ -43,6 +43,8 @@ const start = readlineSync.question('Shall we start? [Y] or [N]: ', {limit: ["Y"
 const playerAlive = readlineSync.question(chalk.yellow(''+playerName+', as you are journeying through the jungle, try not to get eaten 🙈. Please choose from the following options....(hit enter)'))
 
 
+
+
 let isPlayerOptions = true;
 let count = 0;
 while (isPlayerOptions) {
@@ -55,10 +57,10 @@ while (isPlayerOptions) {
                     process.exit()
                     break;
                 case "p":
-                    console.log(chalk.blue(playerName+' has ' + playerHealth + ' HP, and the following items: ' + backpack));
+                    console.log(chalk.blue(playerName+' has ' + playerHealth + ' 🩹 HP, and the following items: ' + backpack));
                     break;
                 case "w":
-                    console.log (chalk.green(playerName+' is walking '));
+                    console.log (chalk.green(playerName+' is walking 👣'));
                     explore()
            }
         }
@@ -84,7 +86,7 @@ while (isPlayerOptions) {
         
         function battle () {
                 const currentAnimal = animalSelector()
-                console.log(chalk.red('You are being chased by ' + currentAnimal.animalName + '!'))
+                console.log(chalk.magenta('You are being chased by ' + currentAnimal.animalName + '!'))
                 readlineSync.setDefaultOptions({limit: ['f', 'r', 'q']})  
                 let runFight = readlineSync.question(chalk.red('Are you going to Fight (f) or Run (r)? Press q to quit. '))
                 if (runFight === 'q') {
@@ -99,13 +101,13 @@ while (isPlayerOptions) {
                 let attack = Math.floor(Math.random() * (obj.maxDamage - obj.minDamage)) + obj.minDamage;
                 let animalAttack = Math.floor(Math.random()* (obj.maxDamage - obj.minDamage));
                 while (playerHealth > 0 && obj.HP > 0) {
-                    let runFight = readlineSync.keyIn(chalk.yellow('Is getting to close! Are you going to fight (f) or Run (r)? ', {limit: ["f","r"]}))  
+                    let runFight = readlineSync.keyIn(obj.animalName +'is getting too close 👀! Are you going to fight [f] or Run [r]?', {limit:["f","r"]})  
                     if (runFight === 'f') {
-                        console.log(chalk.blue('You are fighting back! '));
+                        console.log(chalk.blue('You are fighting back! 👊'));
                         obj.HP = obj.HP - attack
                         console.log(chalk.greenBright('You dealt ' + attack + ' points of damage! ' + obj.animalName + ' currently has ' + obj.HP+ ' health points left.'))
-                        userHealth = playerHealth - animalAttack 
-                        console.log(chalk.red('Watch out for an attack! Oh no- ' + obj.enemyName + ' dealt ' + animalAttack + ' points of damage to you! You currently have ' + playerHealth + ' health points left.'))
+                        playerHealth = playerHealth - animalAttack 
+                        console.log(chalk.whiteBright('It bit you!! ' +obj.animalName + ' dealt ' + animalAttack + ' points of damage to you! You currently have ' + playerHealth + ' health points left.'))
                         if (obj.HP <= 0) {
                             backpack.push(obj.item)
                             console.log(chalk.blue('You defeated ' + obj.animalName + '. Take a look at your inventory to see the new item added! '))
@@ -114,18 +116,20 @@ while (isPlayerOptions) {
                             animal.splice(animal.findIndex(animal => animal.animalName === obj.animalName), 1)
                         } 
                         if (playerHealth <= 0) {
-                            console.log(chalk.red('You have have been eaten by the animal'))
+                            console.log(chalk.red('You have have been eaten by the animal 💀'))
                             gameOver = true
                         }
                         if (animal.length === 0) {
                             hasWon = true;
                             console.log(chalk.green('You traveled safely across the jungle! Conrats!'))
                             console.log('GAME OVER')
+                            process.exit()
                         }
                     } else if (runFight === 'r') {
                         playerHealth = 0
                         gameOver = true
-                        console.log(chalk.red('You have tried to run, but you tripped and got eaten'))
+                        console.log(chalk.red('You have tried to run but you tripped and got eaten 🤯'))
+                        process.exit()
                     }
                 }
             }
