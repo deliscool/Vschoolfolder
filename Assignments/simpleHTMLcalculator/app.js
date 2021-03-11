e4c8971dee2be1c1f336c3d93cf3e94271c1c974
Lizadel Yarisantos <yarisantos@gmail.com>
2021-03-10 20:02:36 -0700

    Fix eventlistener, restructured code in js to meet html

--- Assignments/simpleHTMLcalculator/app.js
+++ Assignments/simpleHTMLcalculator/app.js
@@ -1,27 +1,50 @@
-const form1 = document.additionForm
+//this version contains no output html element tag
+// use flexbox container to have the answer closer to the "="
 
-form1.getElementById("submit1").addEventListener('click', function addButton(e) {
-    e.preventDefault();
-    const num1 = form1.Num1.value;
-    const num2 = form1.Num2.value;
-    form1.output1.value = Number(num1) + Number(num2);
-});
+const form = document["simpleCal"]
 
+const button = document.getElementById("addBtn")
+button.addEventListener("click", function(event){
+    event.preventDefault()
+    console.log("fired")
+    const add1 = form.num1.value
+    const add2 = form.num2.value
+    form.num1.value = ""
+    form.num2.value = ""
 
-const form2 = document.subtractionForm
+    const sum = parseInt(add1) + parseInt(add2)
 
-form2.getElementById("submit2").addEventListener('click', function subButton(e) {
-    e.preventDefault();
-    console.log(document.sub);
-    const num1 = document.sub.Num1.value;
-    const num2 = document.sub.Num2.value;
-    form2.sub.ouput2.value = Number(num1) - Number(num2);
-});
+    const h1 = document.createElement('p')
+    h1.textContent = sum
+    document.getElementById("input1").append(h1)
+})
 
-const form3 = document.multiplicationForm
-form3.getElementById("submit3").addEventListener('click', function mulButton(e) {
-    e.preventDefault();
-    const num1 = document.mul.Num1.value;
-    const num2 = document.mul.Num2.value;
-    form3.mul.output3.value = Number(num1) * Number(num2);
-});
+const subButton = document.getElementById("subBtn")
+subButton.addEventListener("click", function(event){
+    event.preventDefault()
+    const sub1 = form.num3.value
+    const sub2 = form.num4.value
+    form.num3.value = ""
+    form.num4.value = ""
+
+    const sub = sub1 - sub2
+
+    const h2 = document.createElement('p')
+    h2.textContent = sub
+    document.getElementById("input2").append(h2)
+})
+
+const mulButton = document.getElementById("mulBtn")
+mulButton.addEventListener("click", function(event){
+    event.preventDefault()
+    const mul1 = form.num5.value
+    const mul2 = form.num6.value
+    // form.num5.value = ""
+    // form.num6.value = ""
+
+    const mul = mul1 * mul2
+
+    const p3 = document.createElement("p")
+    p3.innerText = mul
+    document.getElementById("input3").append(p3)
+})
