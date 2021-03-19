40185e96ba89847f150e2c11d680dbc90070ba7a
Lizadel Yarisantos <yarisantos@gmail.com>
2021-03-18 22:22:55 -0600

    Write code for js clicker and html

--- Assignments/goSpeedClicker/index.js
+++ Assignments/goSpeedClicker/index.js
@@ -1,1 +1,13 @@
+let clicker = 0
 
+if (localStorage.getItem('click') !==null) {
+	clicker = Number(localStorage.click);
+}
+
+const pageClicked = () => {
+	numClicked++;
+	localStorage.SetItem("click", numClicked);
+	console.log(numClicked);
+}
+
+window.addEventlistener("click", pageClicked);
