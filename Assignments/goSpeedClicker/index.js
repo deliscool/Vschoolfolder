4bf0ac8cd5c9270d1a2c8beff5147280a639164a
Lizadel Yarisantos <yarisantos@gmail.com>
2021-03-22 17:46:27 -0600

    Completed gospeed clicker

--- Assignments/goSpeedClicker/index.js
+++ Assignments/goSpeedClicker/index.js
@@ -1,13 +1,18 @@
-let clicker = 0
+let img = document.getElementById("fruit")
+img.addEventListener("click", clickCount)
 
-if (localStorage.getItem('click') !==null) {
-	clicker = Number(localStorage.click);
+let count = 0
+localStorage.getItem(numberofClicks)
+
+if("numberOfClicks" === 0) {
+	document.getElementById("clicks").innerHTML = 0
+} elese ;{
+	document.getElementById.innerHTML = localStorage.getItem ("numberofClicks")
+}	
+function clickCount() {
+	count += 1
+	localStorage.setItem("numberofClicks", count)
+	document.getElementById("clicks").innerHTML = localStorage.getItem("numberofClicks")
+	console.log(count)
+
 }
-
-const pageClicked = () => {
-	numClicked++;
-	localStorage.SetItem("click", numClicked);
-	console.log(numClicked);
-}
-
-window.addEventlistener("click", pageClicked);
