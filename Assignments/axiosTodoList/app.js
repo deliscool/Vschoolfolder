7557ba35eca91a4f58b90824af94559d775d32dc
Lizadel Yarisantos <yarisantos@gmail.com>
2021-05-04 15:09:20 -0600

    Start Template for Axios To do Capstone

---
+++ Assignments/axiosTodoList/app.js
@@ -0,0 +1,10 @@
+// todo lists:
+// accepts input from a form
+// --> create a form
+// title, desc, img
+
+//get todo items from api
+// --> add those items to the DOM
+// title, dec, img
+//CHECKBOX --> checked for completed items
+// make put a request to mark items
