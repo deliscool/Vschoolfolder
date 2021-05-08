ad513167da51f5551246246a89c2b36b04bae26c
Lizadel Yarisantos <yarisantos@gmail.com>
2021-05-08 15:20:32 -0600

    Edits

--- Assignments/pokemonList/index.js
+++ Assignments/pokemonList/index.js
@@ -4,7 +4,7 @@
 const xhr = new XMLHttpRequest()
 
 xhr.open("GET", "https://api.vschool.io/pokemon", true)
-xhr.send()
+xhr.send();
 
 xhr.onreadystatechange = function () {
     if(this.readyState === 4 && status ===200) {
@@ -18,13 +18,15 @@ xhr.onreadystatechange = function () {
 
 //Step Two Make each Pokemon's name show up on a separate line in the browser. You will be using a for loop to iterate through each pokemon object, and some DOM mamnipulation to add nodes for each Pokemon.
 
-function displayData (arr){
-    for(let i =0 ; i < arr.length ; i ++){
-        const paragraph = document.createElement("p")
-        paragraph.textContent = `${arr[i].name} with a description of ${arr[i].resource_url}`
-        document.body.appendChild(p)
+function showData(arr){
+    for(let i = 0; i < arr.length; i++){
+        const h1 = document.createElement('h1')
+        const h2 = document.createElement('h2')
+        h1.textContent = `This Pokemon's name is ${arr[i].name}`
+        h2.textContent = `You can find more info about them at ${arr[i].url}`
+        document.body.appendChild(h1)
+        document.body.appendChild(h2)
     }
-}
+}        
 
-
-//Extra Credit Extra Credit Display details about each pokemon
+//Extra Credit Extra Credit Display details about each pokemon.
---
+++ Assignments/pokemonList/main.js
@@ -0,0 +1,17 @@
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send();
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const jsonData = xhr.responseText;
+        const parse = JSON.parse(jsonData)
+        showData(parse.objects[0].pokemon)
+    }
+}
+let body = document.querySelector("div")
+function showData(thing) {
+    for (let i = 0; i < thing.length; i++) {
+        const h1 = document.createElement('h1')
+        h1.innerText = thing[i].name;
+        document.body.appendChild(h1)
+    }
+}
